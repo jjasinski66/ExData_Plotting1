0c4d62637b6c78099db021e06f0856e529f0790c
Jerry Jasinski <jjasinski66@gmail.com>
2015-04-11 17:04:40 -0500

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,25 @@
+## Load data.table library
+library(data.table)
+library(chron)
+library(ggplot2)
+
+DT <- fread("household_power_consumption.txt", na.strings = "?")
+DT <- DT[,Datetype := as.Date(Date, "%d/%m/%Y")]
+
+## Subset only the two days we are looking at.
+sub <- subset(DT, Datetype == '2007-02-01' | Datetype == '2007-02-02')
+
+## create timestamp column to comparable Date format
+sub <- sub[,Timestamp := paste(Datetype, Time, sep = " ")] 
+sub$Timestamp <- as.POSIXct(sub$Timestamp,  format="%Y-%m-%d %H:%M:%S")
+
+## convert Global_active_power to numeric
+sub <- sub[,Global_active_power := as.numeric(Global_active_power)]
+
+## plot to png device
+plot(sub$Timestamp, sub$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+
+dev.copy(png, file = "plot2.png")
+
+dev.off()
+
