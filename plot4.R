6910434d9356071705fb8386e170bdc8e94c3f3e
Jerry Jasinski <jjasinski66@gmail.com>
2015-04-11 19:40:46 -0500

    formatting

--- plot4.R
+++ plot4.R
@@ -31,8 +31,8 @@ plot(sub$Timestamp, sub$Sub_metering_1, type = 'n', ylab = "Energy sub metering"
 points(sub$Timestamp, sub$Sub_metering_1, type = "l")
 points(sub$Timestamp, sub$Sub_metering_2, type = "l", col= "red")
 points(sub$Timestamp, sub$Sub_metering_3, type = "l", col= "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1),
-       lwd=c(2.5,2.5),col=c("black", "red", "blue"), cex = .25)
+legend("topright", bty = "n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1),
+       lwd=c(1,1, 1), col=c("black", "red", "blue"), cex =.25)
 
 
 plot(sub$Timestamp, sub$Global_reactive_power, type = "l", ylab = "Global reactive power", xlab = "")
